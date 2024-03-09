f5a9f390d6ae78eac673af736048a5bfe2ef2eff
dlrubin <dlrubin@ucsc.edu>
2024-03-08 20:09:59 -0800

    Add basic sky sphere to contain windmill scene

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // prog5: Feel Good JS
-// Written by Dakota Rubin (Student ID: 1595408)
+// Written by Kimberly Ho (Student ID: 1812644)
+// and Dakota Rubin (Student ID: 1595408)
 // CSE 160 (Winter 2024)
 
 import * as THREE from 'three';
@@ -14,14 +15,35 @@ import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 function animate() {
   // Retrieve the next frame to show after the screen refreshes
   // Pauses animation when the user clicks away from this tab in their browser
-	requestAnimationFrame(animate);
+  requestAnimationFrame(animate);
 
   // Rotate the cube by a small amount every frame
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
 
   // Draw the updated scene
-	renderer.render(scene, camera);
+  renderer.render(scene, camera);
+}
+
+// This function makes a sky sphere that will contain the windmill scene
+function createSkySphere() {
+  // The parameters of SphereGeometry: radius, widthSegments and heightSegments
+  const skySphere = new THREE.SphereGeometry(1000, 32, 16);
+
+  // Instantiate a texture loader and load the sky sphere texture
+  const skySphereTexture = new THREE.TextureLoader().load("./images/skytown.png");
+
+  // Use the sky sphere texture for material creation 
+  const skySphereMaterial = new THREE.MeshBasicMaterial({ map:skySphereTexture });
+
+  // Create a mesh of the sky sphere using its geometry and material
+  const skySphereMesh = new THREE.Mesh(skySphere, skySphereMaterial);
+
+  // Place the texture on the inside of the sky sphere
+  skySphereMesh.material.side = THREE.BackSide;
+
+  // The sky sphere is placed at (0, 0, 0) by default
+  scene.add(skySphereMesh);
 }
 
 // ============================================================================
@@ -45,6 +67,9 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 // Add the renderer element to the HTML document
 document.body.appendChild(renderer.domElement);
 
+// Make a sky sphere that will contain the windmill scene
+createSkySphere();
+
 // Create a cube mesh using the given vertices and material color
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
@@ -53,7 +78,7 @@ const cube = new THREE.Mesh(geometry, material);
 // The cube is placed at coordinates (0, 0, 0) by default
 scene.add(cube);
 
-// Move the camera outward to show the cube
+// Move the camera to show different parts of the map and objects
 camera.position.z = 5;
 
 // Call the main loop to redraw the scene every time the screen refreshes
